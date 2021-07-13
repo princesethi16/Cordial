d39faf89b437081ba6ecd2bc9c9be18677b9245c
Prince Sethi <sethiprince007@gmail.com>
2021-07-14 00:33:39 +0530

    Set individual styles to the pages

--- index.js
+++ index.js
@@ -9,7 +9,15 @@ const app = express();
 app.set('view engine','ejs');
 app.set('views',path.join(__dirname,'views'));
 
+// set up the middleware for the static files like css images frontend javascript and images 
+app.use(express.static('./static'));
+ 
+
+//middleware for the layouts
 app.use(expressEjsLayouts);
+// to extract the style and script and place in appropriate place in html document
+app.set('layout extractStyles',true);
+app.set('layout extractScripts',true);
 
 // import the router module for use
 var router = require('./routes/home');
