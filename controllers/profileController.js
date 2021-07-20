3c688a6368f8f8d0dd33c5934ef13aae52c51a94
Prince Sethi <sethiprince007@gmail.com>
2021-07-20 17:58:40 +0530

    make profile info editable

--- controllers/profileController.js
+++ controllers/profileController.js
@@ -2,12 +2,31 @@
 const User = require('../models/userSchema');
 
 module.exports.profile = (req,res)=>{
-    let userEmail = req.params.user;
-    User.findOne({email: userEmail},(err,foundUser)=>{
+    let userId = req.params.user;
+    User.findOne({_id: userId},(err,foundUser)=>{
         return res.render('profile',{
             title: "Profile",
             currUser: foundUser
         });
 
     });
+};
+
+module.exports.editProfile = (req,res)=>{
+    let userId = req.params.user;
+    if(req.user.id == userId){
+        let userName = req.body.name;
+        let userEmail = req.body.email;
+        
+        User.findByIdAndUpdate(userId,{
+            name: userName,
+            email: userEmail
+        },(err,user)=>{
+            return res.redirect('back');
+        });
+
+    }
+    else{
+        return res.redirect('back');
+    }
 };
--- routes/profile.js
+++ routes/profile.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const router = express.Router();
+const passport = require('passport');
 
 var profileController = require('../controllers/profileController');
 router.get('/:user', profileController.profile);
+
+router.post('/edit/:user',passport.checkAuthentication,profileController.editProfile);
 
 module.exports = router;
